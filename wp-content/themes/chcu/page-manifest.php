ad11806606024cf04d64078ee3ac31255652fbff
Jan Jablunka <jablunka@gmail.com>
2014-07-31 18:46:03 +0200

    incremental

---
+++ wp-content/themes/chcu/page-manifest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Manifest
+*/
+
+add_theme_support('post-thumbnails');
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+
+		<div class="row pageHeader">
+			<div class="col-xs-12">
+				<h1>
+					Manifest
+				</h1>
+			</div>
+			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8">
+				<p class="lead">
+					bla bla bla
+				</p>
+			</div>
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>

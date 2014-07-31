c0bd284f4cede2752a939bba233b8919b1a438fa
Jan Jablunka <jablunka@gmail.com>
2014-07-31 19:07:28 +0200

    incremental

--- wp-content/themes/chcu/page-manifest.php
+++ wp-content/themes/chcu/page-manifest.php
@@ -17,11 +17,6 @@ add_theme_support('post-thumbnails');
 					Manifest
 				</h1>
 			</div>
-			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8">
-				<p class="lead">
-					O co vám jde?
-				</p>
-			</div>
 		</div>
 
 		<div class="row">

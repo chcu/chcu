1b18736ad112deaf6a9c84972cc1d777904b0583
Jan Jablunka <jablunka@gmail.com>
2014-08-01 12:20:36 +0200

    incremental

--- wp-content/themes/chcu/page-bodProgramu.php
+++ wp-content/themes/chcu/page-bodProgramu.php
@@ -5,20 +5,19 @@ Template Name: BodProgramu
 
 add_theme_support('post-thumbnails');
 
+while (have_posts()) {
+	the_post();
+}
+
 ?>
 
 <?php get_header(); ?>
 
 	<div class="container">
 
-		<div class="row pageHeader">
+		<div class="row">
 			<div class="col-xs-12">
-				<h1>
-					Kontakt
-				</h1>
-				<p class="lead">
-					Kde jste vy, byli jsme i my, kde jsme my, budete i vy.<br />A volejte Hollanovi.
-				</p>
+				<?php echo the_content() ?>
 			</div>
 		</div>
 

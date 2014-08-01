e7ec7e6c9fac4d79241fbf2231abf9a36cb54516
Jan Jablunka <jablunka@gmail.com>
2014-08-01 12:15:05 +0200

    incremental

---
+++ wp-content/themes/page-bodProgramu.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: BodProgramu
+*/
+
+add_theme_support('post-thumbnails');
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+
+		<div class="row pageHeader">
+			<div class="col-xs-12">
+				<h1>
+					Kontakt
+				</h1>
+				<p class="lead">
+					Kde jste vy, byli jsme i my, kde jsme my, budete i vy.<br />A volejte Hollanovi.
+				</p>
+			</div>
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>

2c22f161fbd8a4102cb59190ebab418da08ca5f5
Jan Jablunka <jablunka@gmail.com>
2014-08-19 20:45:31 +0200

    incremental

--- wp-content/themes/chcu/page-bodProgramu.php
+++ wp-content/themes/chcu/page-bodProgramu.php
@@ -7,6 +7,34 @@ add_theme_support('post-thumbnails');
 
 while (have_posts()) {
 	the_post();
+	$postId = get_the_ID();
+
+	$meta = get_post_meta($postId);
+}
+
+$programStories = get_pages(array(
+	'hierarchical' => 0,
+	'sort_column'  => 'menu_order',
+	'meta_key'     => 'pageRole',
+	'meta_value'   => 'programStory',
+));
+
+foreach ($programStories as $key => $programStory) {
+	if ($programStory->ID == $postId) {
+		$storyKey = $key;
+	}
+}
+
+if ($storyKey - 1 >= 0) {
+	$previousStory = $programStories[$storyKey - 1];
+} else {
+	$previousStory = $programStories[count($programStories) - 1];
+}
+
+if ($storyKey + 1 < count($programStories)) {
+	$nextStory = $programStories[$storyKey + 1];
+} else {
+	$nextStory = $programStories[0];
 }
 
 ?>
@@ -16,8 +44,25 @@ while (have_posts()) {
 	<div class="container">
 
 		<div class="row">
-			<div class="col-xs-12">
+			<div class="programStoryHeader col-md-offset-1 col-md-10 col-sm-12" data-topic="<?php echo $meta['topic'][0] ?>">
+				<h1>
+					<?php the_title() ?>
+				</h1>
+				<p class="lead">
+					<?php echo $meta['perex'][0] ?>
+				</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-offset-1 col-md-2">
+				<a href="<?php echo get_permalink($previousStory) ?>">Chcu předchozí</a>
+			</div>
+			<div class="col-md-6">
 				<?php echo the_content() ?>
+			</div>
+			<div class="col-md-2">
+				<a href="<?php echo get_permalink($nextStory) ?>">Chcu další</a>
 			</div>
 		</div>
 

bde82729eadee67760cf4ec85a908b6fda5707d7
Jan Jablunka <jablunka@gmail.com>
2014-08-25 15:44:48 +0200

    incremental

--- wp-content/themes/chcu/page-kontakt.php
+++ wp-content/themes/chcu/page-kontakt.php
@@ -21,6 +21,32 @@ add_theme_support('post-thumbnails');
 				</p>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-sm-offset-3 col-sm-2 contactSegment meetings">
+				<p class="image">
+					<img class="img-responsive" src="<?php echo get_theme_root_uri() . '/' . get_template() ?>/icon_cup.png" />
+				</p>
+				<p>
+					Každé pondělí<br />16&ndash;19&nbsp;h<br />v&nbsp;kavárně <a target="_blank" href="http://www.cafeatlas.cz/">Atlas</a>
+				</p>
+			</div>
+			<div class="col-sm-2 contactSegment">
+				<p class="image">
+					<img class="img-responsive" src="<?php echo get_theme_root_uri() . '/' . get_template() ?>/icon_flag.png" />
+				</p>
+				<p>
+					Žít Brno<br />Zemědělská 72<br />613&nbsp;00 Brno
+				</p>
+			</div>
+			<div class="col-sm-2 contactSegment">
+				<p class="image">
+					<img class="img-responsive" src="<?php echo get_theme_root_uri() . '/' . get_template() ?>/icon_phone.png" />
+				</p>
+				<p>
+					Matěj Hollan<br />jednička<br />774 956 148
+				</p>
+			</div>
+		</div>
 
 	</div>
 

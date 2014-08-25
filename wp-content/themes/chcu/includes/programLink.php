7076457c4f56fea442f12a393f4cd2511b05af3b
Jan Jablunka <jablunka@gmail.com>
2014-08-25 16:33:10 +0200

    incremental

--- wp-content/themes/chcu/includes/programLink.php
+++ wp-content/themes/chcu/includes/programLink.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-xs-12">
 		<p class="text-center">
-			<a class="blackButton" target="_blank" href="https://docs.google.com/document/d/1eX_iXNyVadFlYYRr-mBNaq5ElTHmb2Xz8FQfWOopk5U/edit">Celý černý program na bílém <span class="fa fa-caret-right"></a>
+			<a class="blackButton" target="_blank" href="http://bit.ly/programZitBrno">Celý černý program na bílém <span class="fa fa-caret-right"></a>
 		</p>
 	</div>
 </div>

418744085bc0b1fe52cc0bae8a964773d8b9f7e3
Jan Jablunka <jablunka@gmail.com>
2014-08-25 19:24:13 +0200

    incremental

--- wp-content/themes/chcu/single.php
+++ wp-content/themes/chcu/single.php
@@ -3,45 +3,38 @@
 	<div class="container">
 		<?php while (have_posts()) { the_post(); ?>
 
+			<?php
+			$postId = get_the_ID();
+			$post = get_post($postId);
+			$user = get_user_by('id', $post->post_author);
+			?>
+
 			<div class="row">
-				<div class="col-xs-12">
-					<h1>
+				<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-12">
+					<h1 class="postTitle">
 						<?php the_title(); ?>
 					</h1>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-xs-12">
-					<?php the_post_thumbnail( 'wpbs-featured' ); ?>
+				<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-12">
+					<p class="postDetails">
+						<?php if ($candidatePage[0]) { ?>
+							<a href="<?php echo get_permalink($candidatePage[0]->ID) ?>"><?php echo $user->display_name ?></a>
+						<?php } else { ?>
+							<?php echo $user->display_name ?>
+						<?php } ?>
+						&middot; <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time>
+					</p>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-xs-12">
-					<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12">
+				<div class="col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-12 postContent">
 					<?php the_content(); ?>
 				</div>
 			</div>
-
-			<div class="row">
-				<div class="col-xs-12">
-					<?php wp_link_pages(); ?>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12">
-					<?php the_tags('<p class="tags"><span class="tags-title">' . __("Tags","wpbootstrap") . ':</span> ', ' ', '</p>'); ?>
-				</div>
-			</div>
-
-			<?php comments_template('',true); ?>
 
 		<?php } ?>
 	</div>

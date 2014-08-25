2381b8ab86b3d51e63e9b2309f35522a47de1db4
Jan Jablunka <jablunka@gmail.com>
2014-08-25 15:17:53 +0200

    incremental

--- wp-content/themes/chcu/includes/posts.php
+++ wp-content/themes/chcu/includes/posts.php
@@ -9,14 +9,17 @@
 			'meta_key'     => 'userLogin',
 			'meta_value'   => $user->user_login,
 		));
+		$imageUrl = trim($match[1]);
 		?>
 		<li class="clearfix">
-			<div class="image col-lg-offset-1 col-lg-3 col-md-3 col-sm-5 col-xs-12">
-				<p>
-					<a href="<?php echo get_permalink($post) ?>"><img src="<?php echo $match[1] ?>" class="img-responsive" /></a>
-				</p>
-			</div>
-			<div class="content col-lg-7 col-md-9 col-sm-7 col-xs-12">
+			<?php if ($imageUrl) { ?>
+				<div class="image col-lg-offset-1 col-lg-3 col-md-3 col-sm-5 col-xs-12">
+					<p>
+						<a href="<?php echo get_permalink($post) ?>"><img src="<?php echo $imageUrl ?>" class="img-responsive" /></a>
+					</p>
+				</div>
+			<?php } ?>
+			<div class="content <?php if ($imageUrl) { ?>col-lg-7 col-md-9 col-sm-7 <?php } ?>col-xs-12">
 				<h3 class="title">
 					<a href="<?php echo get_permalink($post) ?>"><?php echo $post->post_title ?></a>
 				</h3>

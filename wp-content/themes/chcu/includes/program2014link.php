c45dad43e147e1e77f808b8e59bac4cf759fa0ed
Jan Jablunka <jablunka@gmail.com>
2014-08-27 14:09:48 +0200

    incremental

---
+++ wp-content/themes/chcu/includes/program2014link.php
@@ -0,0 +1,19 @@
+<?php
+
+$program = get_pages(array(
+	'meta_key'   => 'pageRole',
+	'meta_value' => 'program2014',
+));
+
+var_dump($program); die;
+
+?>
+
+<div class="row">
+	<div class="col-xs-12">
+		<p class="text-center">
+			<a class="blackButton" target="_blank" href="">Celý černý program na bílém <span class="fa fa-caret-right"></a>
+		</p>
+	</div>
+</div>
+

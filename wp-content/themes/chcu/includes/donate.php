31df098bd0137a1e0609419996eca2d7f24579ed
Jan Jablunka <jablunka@gmail.com>
2014-09-02 22:29:12 +0200

    incremental

--- wp-content/themes/chcu/includes/donate.php
+++ wp-content/themes/chcu/includes/donate.php
@@ -8,7 +8,7 @@
 			Svoje prachy uvidíš na našem transparentním účtu: <a class="accent" target="_blank" href="https://www.fio.cz/scgi-bin/hermes/dz-transparent.cgi?ID_ucet=2400634319">2400634319/2010 (Fio banka)</a>
 		</p>
 		<p class="text-center donateButton">
-			<a class="img img-responsive" target="_blank" href="https://www.hithit.com/cs/project/793/chcu-zit-brno"><img class="img-responsive"> src="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/chci_prispet.gif" /></a>
+			<a class="img img-responsive" target="_blank" href="https://www.hithit.com/cs/project/793/chcu-zit-brno"><img class="img-responsive" src="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/chci_prispet.gif" /></a>
 		</p>
 	</div>
 </div>

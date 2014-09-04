33433a3128f2ad916785cb5aa471b71c4377922e
Jan Jablunka <jablunka@gmail.com>
2014-09-04 18:02:59 +0200

    incremental

--- wp-content/themes/chcu/header.php
+++ wp-content/themes/chcu/header.php
@@ -35,9 +35,9 @@
     <meta property="og:url" content="http://chcu.zitbrno.cz" />
     <meta property="og:site_name" content="CHCU ŽÍT BRNO"/>
     <meta property="og:title" content="CHCU ŽÍT BRNO" />
-    <meta property="og:type" content="article" />    
+    <meta property="og:type" content="article" />
     <meta property="og:description" content="Chceme vrátit naději životu v Brně a chceme, aby nám nikdo nebránil být na něj hrdí." />
-    <meta property="og:image" content="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/CHCU_facebook_oimage2_tr.png" />  
+    <meta property="og:image" content="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/CHCU_facebook_oimage2_tr.png" />
 
 
 
@@ -45,11 +45,9 @@
 
 		<link href="//fonts.googleapis.com/css?family=Roboto&subset=latin-ext" rel="stylesheet" type="text/css" />
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
-<<<<<<< HEAD
-     <link rel="shortcut icon" href="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/favicon2.gif" type="image/x-icon" /> 
-=======
-     <link rel="shortcut icon" href="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/favicon.gif" type="image/x-icon" />
->>>>>>> ba96c8ca105c1d2fcb19075b5695c29f44773e05
+
+		<link rel="shortcut icon" href="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/favicon2.gif" type="image/x-icon" />
+
 	</head>
 
 	<body <?php body_class(); ?>>

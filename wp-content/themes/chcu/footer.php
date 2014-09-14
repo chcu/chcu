6a5179ee2b1f45845b90523e52d58de5bca88c56
Jan Jablunka <jablunka@gmail.com>
2014-09-14 09:33:38 +0200

    incremental

--- wp-content/themes/chcu/footer.php
+++ wp-content/themes/chcu/footer.php
@@ -81,7 +81,7 @@
 				<div class="row">
 					<div class="col-lg-5">
 						<p>
-							&copy; <?php echo date('Y') ?> Žít Brno
+							<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
 						</p>
 					</div>
 				</div>

233c7029b2b8fc9e10cfab3af5881b3cbffcc6f4
Jan Jablunka <jablunka@gmail.com>
2014-09-19 11:14:42 +0200

    incremental

--- wp-content/themes/chcu/header.php
+++ wp-content/themes/chcu/header.php
@@ -12,11 +12,7 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-<<<<<<< HEAD
-    
 
-=======
->>>>>>> dc313053d8dba388446460eb346e2179233fcab2
 
 		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
@@ -37,20 +33,7 @@
 		          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
 		<![endif]-->
 
-<<<<<<< HEAD
-    <!--<meta property="og:url" content="http://chcu.zitbrno.cz" />
-    <meta property="og:site_name" content="CHCU ŽÍT BRNO"/>
-    <meta property="og:title" content="CHCU ŽÍT BRNO" />
-    <meta property="og:type" content="article" />
-    <meta property="og:description" content="Chceme vrátit naději životu v Brně a chceme, aby nám nikdo nebránil být na něj hrdí." />
-    <meta property="og:image" content="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/CHCU_facebook_oimage2_tr.png" /> -->
 
-    
-        <meta property="og:image" content="<?php echo get_theme_root_uri() . '/' . get_template() ?>/images/CHCU_facebook_oimage2_tr.png" />
-    
-
-=======
->>>>>>> dc313053d8dba388446460eb346e2179233fcab2
 		<link href="<?php echo get_theme_root_uri() . '/' . get_template() ?>/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 
 		<link href="//fonts.googleapis.com/css?family=Roboto&subset=latin-ext" rel="stylesheet" type="text/css" />

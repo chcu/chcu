215b0470f0ed4ba85876d44a344c3060bcd70816
Jan Jablunka <jablunka@gmail.com>
2014-10-05 21:19:52 +0200

    incremental

---
+++ wp-content/themes/chcu/page-stred.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: CelyProgram
+*/
+
+add_theme_support('post-thumbnails');
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+
+		<div class="row pageHeader">
+			<div class="col-xs-12">
+				<h1>
+					Chcu žít Brno-střed
+				</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-12 longCopy">
+				<?php while (have_posts()) {
+					the_post();
+					the_content();
+				} ?>
+			</div>
+		</div>
+
+		<hr />
+
+		<?php include 'includes/donate.php' ?>
+
+		<?php include 'includes/kolo.php' ?>
+
+	</div>
+
+<?php get_footer(); ?>

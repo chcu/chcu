df4f01d095246399af60762da67a6ff907c1a88a
Jan Pospisil <jsem@posp.cz>
2017-03-05 14:06:51 +0100

    simple sablona

---
+++ wp-content/themes/chcu/page-simple.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Simple
+*/
+
+add_theme_support('post-thumbnails');
+
+?>
+
+<?php get_header(); ?>
+
+	<div class="container">
+
+		<div class="row pageHeader">
+			<div class="col-xs-12">
+				<h1>
+					<?php the_title()?>
+				</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-12 longCopy">
+				<?php while (have_posts()) {
+					the_post();
+					the_content();
+				} ?>
+			</div>
+		</div>
+
+		<hr />
+
+		<?php include 'includes/donate.php' ?>
+
+		<?php include 'includes/kolo.php' ?>
+
+	</div>
+
+<?php get_footer(); ?>
